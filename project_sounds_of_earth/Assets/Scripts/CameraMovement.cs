51f684f997b0bdfef14148b25d0329605d13e5cf
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-18 09:53:30 +0300

    Kamera pozisyonu scriptindeki offset değiştirildi. Temel mantık PDF'si eklendi.

--- project_sounds_of_earth/Assets/Scripts/CameraMovement.cs
+++ project_sounds_of_earth/Assets/Scripts/CameraMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private int interPolationFramesCount = 90;
+    private int interPolationFramesCount = 120;
     int elapsedFrames = 0;
     Vector3 endPosition = new Vector3(0, 0, 0);
+    Vector3 endPosRotation = new Vector3(0, 0, 0);
+    Vector3 focalPoint = new Vector3(0, 0, 0);
+    float cameraDistanceMultiplier = 3;
+    bool positionSet = false;
+
 
     Vector3 cameraOffsetVector = new Vector3(35, 10, 0);
     selectCountries _selectCountries;
@@ -19,21 +24,32 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        changeCameraPosition();
+        
     }
 
-
+    private void LateUpdate()
+    {
+        changeCameraPosition();
+        idleCameraMovement();
+    }
 
     void changeCameraPosition()
     {
-        endPosition = _selectCountries.middleVector + cameraOffsetVector; //aslýnda bu camera offset her zaman iþe yaramaz çünkü dünyanýn arkasýna girdiðimizde + 25,10,0 muhtemelen dünyanýn içine girmesine sebep olur.
-                                                                          //Offseti de conditional yapacaðým. Prototipleme için bu
-        if (transform.position != endPosition)
+        focalPoint = _selectCountries.middleVector;
+        endPosition = focalPoint * cameraDistanceMultiplier;
+        //camera rotation ayarlanacak. Muhtemelen Zelda OOT'den beri kullanýlan Z-Target mekaniði araþtýrýlýp entegre edilecek.
+
+
+        //Offseti de conditional yapacaðým. Prototipleme için bu
+        if (transform.position != endPosition && !positionSet)
         {
             float lerpInterpolationRatio = (float)elapsedFrames / interPolationFramesCount;
             transform.position = Vector3.Lerp(transform.position, endPosition, Mathf.SmoothStep(0.0f, 1.0f, lerpInterpolationRatio));
             elapsedFrames = (elapsedFrames + 1) % (interPolationFramesCount + 1);
         }
+
+        if(transform.position == endPosition)
+            positionSet = true;
     }
 
 
