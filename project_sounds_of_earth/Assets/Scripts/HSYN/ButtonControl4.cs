51127300d76d6a7cf814c434aa8cf0432752eddc
HuseyinTayyip <95476290+HuseyinTayyip@users.noreply.github.com>
2022-05-20 21:29:46 +0300

    Üzerinde çalışılan internetten müzik çekme, dünya dönüşünün ayarlanması ve buton kontrolleri ile alakalı scriptler yüklenmiştir.

---
+++ project_sounds_of_earth/Assets/Scripts/HSYN/ButtonControl4.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonControl4 : MonoBehaviour
+{
+
+    public GameObject button;
+    public GameObject canvas;
+    public void Start()
+    {
+        GameObject newCanvas = Instantiate(canvas) as GameObject;
+        GameObject newButton = Instantiate(button) as GameObject;
+        newButton.transform.SetParent(newCanvas.transform, true);
+        //buton.GetComponent<Button>().onClick.AddListener();
+    }
+    public void OnButtonClick()
+    {
+        Debug.Log(button.transform.Find("Text").GetComponent<Text>().text);
+        /*if (buton.transform.Find("Text").GetComponent<Text>().text=="Almanya")
+        {
+            Debug.Log("You Have The correcct answer");
+        }
+        else
+        {
+            Debug.Log("You have the wrong answeer");
+        }*/
+    }
+}
