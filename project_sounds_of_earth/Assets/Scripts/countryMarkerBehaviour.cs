30189787c13edeca31450ac5e6eca58fd9057310
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-05-22 12:51:19 +0300

    [Bugfix & feature] import edilen scriptlerin sorun çıkarması çözüldü, country markerların kamera uzaklığına göre max boyutları ayarlandı.

--- project_sounds_of_earth/Assets/Scripts/countryMarkerBehaviour.cs
+++ project_sounds_of_earth/Assets/Scripts/countryMarkerBehaviour.cs
@@ -6,29 +6,39 @@ public class countryMarkerBehaviour : MonoBehaviour
 {
 
     private int interPolationFramesCount = 200;
-    private float localScaleUpperbound = 1.99f;
-    private float localScaleLowerbound = 1.02f;
+    private float localScaleUpperbound = 1.49f;
+    private float localScaleLowerbound = 1.01f;
     int elapsedFrames = 0;
     bool _timeToIncreaseSize = true;
+    bool _newScaleSizeCalculated = false;
 
     private Vector3 startingScale = new Vector3(1, 1, 1);
-    private Vector3 endingScale = new Vector3(2, 2, 2);
-   
+    private Vector3 endingScale = new Vector3(1.1f, 1.1f, 1.1f);
+    selectCountries _selectCountries;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _selectCountries = GameObject.Find("CountryMarkers").GetComponent<selectCountries>();
+    }
+ 
     void Update()
     {
+        if(!_newScaleSizeCalculated)
+        calculatedEndScaleAndUpperBound();
+
         changeSize();
     }
 
     void changeSize()
     {
+
+
         if (transform.localScale.z < localScaleUpperbound && _timeToIncreaseSize)
         {
             increaseLocalScale();
 
         }
-        else if (transform.localScale.z >= localScaleLowerbound && !_timeToIncreaseSize)
+        else if (transform.localScale.z > localScaleLowerbound && !_timeToIncreaseSize)
         {
             decreaseLocalScale();
         }
@@ -56,5 +66,39 @@ public class countryMarkerBehaviour : MonoBehaviour
             elapsedFrames = 0;
             _timeToIncreaseSize = true;
         }
+    }
+
+    void calculatedEndScaleAndUpperBound()
+    {
+        if (_selectCountries.magnitudeOfcombinationVector > 78)
+        {
+            endingScale = startingScale * 1.4f;
+        }
+        else if (_selectCountries.magnitudeOfcombinationVector <= 78 && _selectCountries.magnitudeOfcombinationVector > 76)
+        {
+            endingScale = startingScale * 1.6f;
+        }
+        else if (_selectCountries.magnitudeOfcombinationVector <= 76 && _selectCountries.magnitudeOfcombinationVector > 74)
+        {
+            endingScale = startingScale * 1.8f;
+        }
+        else if (_selectCountries.magnitudeOfcombinationVector <= 74 && _selectCountries.magnitudeOfcombinationVector > 72)
+        {
+            endingScale = startingScale * 2.0f;
+        }
+        else if (_selectCountries.magnitudeOfcombinationVector <= 72 && _selectCountries.magnitudeOfcombinationVector > 70)
+        {
+            endingScale = startingScale * 2.2f;
+        }
+        else
+        {
+            endingScale = startingScale * 2.4f;
+        }
+
+        localScaleUpperbound = endingScale.x -0.01f;
+
+        Debug.Log("Magnitude of comb vector: "+ _selectCountries.magnitudeOfcombinationVector+ "ending scale: " + endingScale + " localScaleBOund: " + localScaleUpperbound) ;
+
+        _newScaleSizeCalculated = true;
     }
 }
