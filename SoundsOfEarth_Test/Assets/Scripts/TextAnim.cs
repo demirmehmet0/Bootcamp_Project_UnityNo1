4b73c186994b4740f9cb0d54c4a178b97a89ea8a
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2022-05-28 05:23:41 +0300

    TextAnim hakkında yorum düzeltmeleri

--- SoundsOfEarth_Test/Assets/Scripts/TextAnim.cs
+++ SoundsOfEarth_Test/Assets/Scripts/TextAnim.cs
@@ -8,20 +8,20 @@ public class TextAnim : MonoBehaviour
     //<param name="_Text">
     //Bu scriptin atandığı text objesini tutmak için oluşturulan değişken
     //</param>
-    private bool isUp = true;
-    //<param name="isUp">
+    private bool isUpOrDown = true;
+    //<param name="isUpOrDown">
     //Textin font boyutu artacak mı azalacak mı olduğunu belirleyen bool
     //default olarak true, textin fontu belirlenen değerden(32) küçük ise true büyük ise false değerini alır
     //</param>
-    private bool isLoading = false;
+    private bool isLoadingAnim = false;
     //<param name="isLoading">
     //Scriptin atıldıpı textin yükleniyor animi mi(3 nokta) yoksa
     //büyüyüp küçülme animi mi olduğunu belirleyen bool(text oyun
     //bekleniyor ise 3 nokta değil ise büyütüp küçültme)
     //default olarak false, text oyun bekleniyor ise true değil ise false değerini alır 
     //</param>
-    private bool is3Dot = false;
-    //<param name="is3Dot"> 
+    private bool is3DotDone = false;
+    //<param name="is3DotDone"> 
     //text üzerinde 3 nokta eklemesi yapılırken 3 adet nokta var mı yok mu kontrolünü sağlayan bool
     //default olarak false, 3 nokta eklenilene kadar false ve 3 nokta eklenmiş ise true değerini alır
     //</param>
@@ -31,39 +31,53 @@ public class TextAnim : MonoBehaviour
     //default olarak 0dır yani animasyon yoktur ve sıfırdan büyük değerler alır
     //</param>
 
+
+    //MAIN FUNCTIONS
     private void Awake()//Script yüklendiği an çalışacak olan default fonksiyon
     {
         _Text = GetComponent<TMP_Text>();//Scriptin atandığı objeden text objesinin _Text değişkenine atanması
         if (_Text.text == "Oyun Bekleniyor")//_Text "oyun bekleniyor" ise
-        { isLoading = true; StartCoroutine("Loading"); }//isLoading true yapılır ve Loading fonksiyonu coroutine olarak başlatılır
+        { isLoadingAnim = true; StartCoroutine("Loading3DotAnim"); }//isLoadingAnim true yapılır ve Loading3DotAnim fonksiyonu coroutine olarak başlatılır
+        else { TextGrowAnim(); }//Yazıyı büyültüp küçülten animasyonunun fonksiyon çağrımı 
     }
 
-    IEnumerator Loading()//Bekleme yapılması için coroutine olarak tanımlanan Loading fonksiyonu
+    void Update()//her bir frame için çalışacak olan default fonksiyon
     {
-    start:
+
+
+    }
+    //MAIN FUNCTIONS
+
+    //COROUTINE FUNCTIONS
+    IEnumerator Loading3DotAnim()//Bekleme yapılması için coroutine olarak tanımlanan Loading fonksiyonu
+    {
+    Start:
         yield return new WaitForSeconds(0.3f);//0.3 saniye beklenmesi sağlanılan komut
-        if (_Text.text.Contains("Oyun Bekleniyor") && _Text.text != "Oyun Bekleniyor..." && !is3Dot)//koşullar sağlanırsa text değişkeninin tuttuğu
+        if (_Text.text.Contains("Oyun Bekleniyor") && _Text.text != "Oyun Bekleniyor..." && !is3DotDone)//koşullar sağlanırsa text değişkeninin tuttuğu
             _Text.text += ".";//yazı parametresinin sonuna . eklemesi yapıyor
         else//değilse
         {
             if (_Text.text.Contains("Oyun Bekleniyor"))//koşulllar sağlanır ise
             {
                 _Text.text = _Text.text.Remove(_Text.text.Length - 1);//text değişkeninin text parametresinin en son karakterini siler
-                is3Dot = true;//3 nokta olduğunu anlamamız için oluşturulan değişken true yapılır
-                if (_Text.text == "Oyun Bekleniyor") is3Dot = false;//bütün noktalar silinmiş ise 3 nokta kontrol değişkenini false yapar
-            } 
+                is3DotDone = true;//3 nokta olduğunu anlamamız için oluşturulan değişken true yapılır
+                if (_Text.text == "Oyun Bekleniyor") is3DotDone = false;//bütün noktalar silinmiş ise 3 nokta kontrol değişkenini false yapar
+            }
         }
-        goto start;//sürekli tekrar etmesi için başa döndürme komutu
+        goto Start;//sürekli tekrar etmesi için başa döndürme komutu
     }
+    //COROUTINE FUNCTIONS
 
-    void Update()//her bir frame için çalışacak olan default fonksiyon
+    //FUNCTIONS
+    private void TextGrowAnim()//Yazıyı büyültüp küçülten fonksiyon
     {
-        if (!isLoading)//tanımlanmış olan text değişkeni loading değil ise
+    Start:
+        if (!isLoadingAnim)//tanımlanmış olan text değişkeni loading değil ise
         {
-            if (_Text.fontSize >= 32 && isUp) { isUp = false; }//font boyutu 32den büyük ve büyütme değişkeni true ise false yapıyor ve küçültme işlemine geçiyor
-            else if (_Text.fontSize <= 28 && !isUp) { isUp = true; }//değilse ve font boyutu 28den küçükse ve büyütme değişkeni false ise true yapıyor ve büyütme işlemine geçiyor
+            if (_Text.fontSize >= 32 && isUpOrDown) { isUpOrDown = false; }//font boyutu 32den büyük ve büyütme değişkeni true ise false yapıyor ve küçültme işlemine geçiyor
+            else if (_Text.fontSize <= 28 && !isUpOrDown) { isUpOrDown = true; }//değilse ve font boyutu 28den küçükse ve büyütme değişkeni false ise true yapıyor ve büyütme işlemine geçiyor
 
-            if (isUp)//büyütme true ise
+            if (isUpOrDown)//büyütme true ise
             {
                 _Text.fontSize += AnimVal * Time.deltaTime;//font boyutuna ekleme yapılıyor
             }
@@ -72,5 +86,8 @@ public class TextAnim : MonoBehaviour
                 _Text.fontSize -= AnimVal * Time.deltaTime;//değil ise azaltma yapılıyor
             }
         }
+        goto Start;
     }
+    //FUNCTIONS
+
 }
