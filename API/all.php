48f6de3d82b9a996b3489d086f858112598425e2
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2022-05-29 20:57:13 +0300

    Bütün kayıtları listeleyen php scripti
    Formatlama işlemi

--- API/all.php
+++ API/all.php
@@ -1,26 +1,20 @@
-<?php 
+<?php
 $servername = "server";
 $username = "user";
-$password = "pass";  
-// Create connection
-$conn = new mysqli($servername, $username, $password,"userdb");
-
-// Check connection
+$password = "pass";
+$conn = new mysqli($servername, $username, $password, "userdb");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+} else {
+  $sql = "Select * FROM Entries";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+      echo urldecode($row["EntryID"]) . ";" . urldecode($row["text"]) . ";" . urldecode($row["country"]) . ";" . urldecode($row["textonly"]) . ";" . urldecode($row["textlatin"]) . ";" . urldecode($row["translated"]) . "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
 }
-else{
-    $sql = "Select * FROM Entries";
-    $result = $conn->query($sql); 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-          echo urldecode($row["EntryID"]). ";" . urldecode($row["text"]). ";" . urldecode($row["country"]). ";" . urldecode($row["textonly"]).";".urldecode($row["textlatin"]).";".urldecode($row["translated"])."<br>";
-        }
-      } else {
-        echo "0 results";
-      }
-}
-
-$conn->close(); 
-?>
+$conn->close();
