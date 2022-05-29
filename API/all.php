22ef042407ef6010c1695db6a522823f8b6e9636
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2022-05-30 00:31:22 +0300

    Bug fix
    +API üzerine urlencode edilmeden gönderildiği için bug oluşması fixlendi
    +API üzerinden gelen dil kodunun 2.parametresi translate üzerinde etkisiz olduğu için hata vermesi fixlendi(ar-xa kabul etmiyor ar kabul ediyor)

--- API/all.php
+++ API/all.php
@@ -1,20 +1,26 @@
-<?php
+<?php 
 $servername = "server";
 $username = "user";
-$password = "pass";
-$conn = new mysqli($servername, $username, $password, "userdb");
+$password = "pass";  
+// Create connection
+$conn = new mysqli($servername, $username, $password,"userdb");
+
+// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} else {
-  $sql = "Select * FROM Entries";
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-      echo urldecode($row["EntryID"]) . ";" . urldecode($row["text"]) . ";" . urldecode($row["country"]) . ";" . urldecode($row["textonly"]) . ";" . urldecode($row["textlatin"]) . ";" . urldecode($row["translated"]) . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
 }
-$conn->close();
+else{
+    $sql = "Select * FROM Entries";
+    $result = $conn->query($sql); 
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo $row["EntryID"]. ";" . $row["text"]. ";" . $row["country"]. ";" . $row["textonly"].";".$row["textlatin"].";".$row["translated"].";".explode("-",$row["country"])[0]."<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+}
+
+$conn->close(); 
+?>
